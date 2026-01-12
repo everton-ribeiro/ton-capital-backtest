8987a39242a64b3520c30939c8a8ff01a6a8b388
Everton Ribeiro <everton.silva.ribeiro@gmail.com>
2026-01-12 16:18:23 -0300

    chore: create config git commit

---
+++ .commitlintrc.ts
@@ -0,0 +1,27 @@
+import type { UserConfig } from "@commitlint/types";
+import { RuleConfigSeverity } from "@commitlint/types";
+
+const typeEnum = [
+	"build",
+	"chore",
+	"ci",
+	"docs",
+	"feat",
+	"fix",
+	"perf",
+	"refactor",
+	"revert",
+	"style",
+	"test",
+];
+
+const Configuration: UserConfig = {
+	extends: ["@commitlint/config-conventional"],
+	parserPreset: "conventional-changelog-atom",
+	formatter: "@commitlint/format",
+	rules: {
+		"type-enum": [RuleConfigSeverity.Error, "always", [...typeEnum]],
+	},
+};
+
+export default Configuration;
